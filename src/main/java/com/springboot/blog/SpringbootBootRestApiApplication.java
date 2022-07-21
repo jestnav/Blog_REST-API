009462cf93d71a9dd451415a9dd0cf5d23e68bf5
Jordi <jordi95e.t@gmail.com>
2022-07-21 17:23:47 +0200

    Project prepared:     1. Project created with the dependencies (using spring initializr)     2. Adding the connection with the DB to the properties file     3. Testing the app with the new properties (connection with the DB)

---
+++ src/main/java/com/springboot/blog/SpringbootBootRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBootRestApiApplication.class, args);
+	}
+}
