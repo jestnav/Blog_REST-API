82217f76aada9e74bcbde18d364dce3a03124ca8
Jordi <jordi95e.t@gmail.com>
2022-07-22 16:44:30 +0200

    DELETE request created for delete a post from the blog given its id     TESTED with Postman

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,13 @@ public class PostController {
 		PostDto postResponse = postService.updatePost(postDto, id);
 		
 		return new ResponseEntity<>(postResponse, HttpStatus.OK);
-	
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletePost(@PathVariable(name="id") long id){
+		
+		postService.deletePostById(id);
+		return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -61,6 +61,12 @@ public class PostServiceImpl implements PostService{
 		return mapToDTO(updatedPost);
 	}
 	
+	@Override
+	public void deletePostById(long id) {
+		Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+		postRepository.delete(post);
+	}
+	
 	
 	// convert entity to DTO
 	private PostDto mapToDTO(Post post) {
